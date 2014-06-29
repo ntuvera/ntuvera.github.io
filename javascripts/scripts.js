0482cd3274fdfba157c9fcca9cf76aead1450aa3
ntuvera <ntuvera@gmail.com>
2014-06-29 14:22:30 -0400

    contacts done, need intro/home

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,6 +1,36 @@
 
 $(function(){
 
+  $('.about-me').hide();
+  $('.projects').hide();
+  $('.contact-me').hide();
 
+  $('#home').hover(function(){
+    $('.projects').hide();
+    $('.contact-me').hide();
+    $('.about-me').hide();
+    $('.home').fadeIn('fast')
+  })
+
+  $('#about-me').hover(function(){
+    $('.home').hide();
+    $('.projects').hide();
+    $('.contact-me').hide();
+    $('.about-me').fadeIn('fast')
+  })
+
+  $('#projects').hover(function(){
+    $('.home').hide();
+    $('.about-me').hide();
+    $('.contact-me').hide();
+    $('.projects').fadeIn('fast')
+  })
+
+  $('#contact-me').hover(function(){
+    $('.home').hide();
+    $('.about-me').hide();
+    $('.projects').hide();
+    $('.contact-me').fadeIn('fast')
+  })
 
 })
