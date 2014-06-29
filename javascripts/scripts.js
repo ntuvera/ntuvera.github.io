e8972b76fcd0f30937632f4b6f6a7ec27bb29aeb
ntuvera <ntuvera@gmail.com>
2014-06-29 19:51:12 -0400

    completely different direction

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,6 +1,6 @@
 
 $(function(){
-
+/* need to re-factor to slim down */
   $('.about-me').hide();
   $('.projects').hide();
   $('.contact-me').hide();
@@ -13,24 +13,27 @@ $(function(){
   })
 
   $('#about-me').hover(function(){
-    $('.home').hide();
-    $('.projects').hide();
-    $('.contact-me').hide();
+    $('.home').fadeOut('fast');
+    $('.projects').fadeOut('fast');
+    $('.contact-me').fadeOut('fast');
     $('.about-me').fadeIn('fast')
   })
 
   $('#projects').hover(function(){
-    $('.home').hide();
-    $('.about-me').hide();
-    $('.contact-me').hide();
+    $('.home').fadeOut('fast');
+    $('.about-me').fadeOut('fast');
+    $('.contact-me').fadeOut('fast');
     $('.projects').fadeIn('fast')
   })
 
   $('#contact-me').hover(function(){
-    $('.home').hide();
-    $('.about-me').hide();
-    $('.projects').hide();
+    $('.home').fadeOut('fast');
+    $('.about-me').fadeOut('fast');
+    $('.projects').fadeOut('fast');
     $('.contact-me').fadeIn('fast')
   })
 
+
+  $('#ghost-1').hide().fadeIn(1000);
+  $('#ghost-2').hide().fadeIn(2000);
 })
