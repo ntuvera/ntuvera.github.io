4b1dab85ace76f4f68e9295c4c45aff7026b85ea
ntuvera <ntuvera@gmail.com>
2014-07-29 10:38:30 -0400

    reformat nav bar, contact links open in new tab

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -5,35 +5,35 @@ $(function(){
   $('.projects').hide();
   $('.contact-me').hide();
 
-  $('#home').hover(function(){
+  $('#home').on("click", function(){
     $('.projects').hide();
     $('.contact-me').hide();
     $('.about-me').hide();
     $('.home').fadeIn('fast')
-  })
+  });
 
-  $('#about-me').hover(function(){
+  $('#about-me').on("click", function(){
     $('.home').fadeOut('fast');
     $('.projects').fadeOut('fast');
     $('.contact-me').fadeOut('fast');
     $('.about-me').fadeIn('fast')
-  })
+  });
 
-  $('#projects').hover(function(){
+  $('#projects').on("click", function(){
     $('.home').fadeOut('fast');
     $('.about-me').fadeOut('fast');
     $('.contact-me').fadeOut('fast');
     $('.projects').fadeIn('fast')
-  })
+  });
 
-  $('#contact-me').hover(function(){
+  $('#contact-me').on("click", function(){
     $('.home').fadeOut('fast');
     $('.about-me').fadeOut('fast');
     $('.projects').fadeOut('fast');
     $('.contact-me').fadeIn('fast')
-  })
+  });
 
 
   $('#ghost-1').hide().fadeIn(1000);
   $('#ghost-2').hide().fadeIn(2000);
-})
+});
